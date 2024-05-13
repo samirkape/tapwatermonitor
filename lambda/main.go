a7a1a27d8390e357c794d90091e9404cbf421159
Samir Kape <samirkape31@gmail.com>
2024-05-13 21:58:53 +0530

    chore: basic refactor

--- lambda/main.go
+++ lambda/main.go
@@ -8,17 +8,16 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"github.com/supabase-community/supabase-go"
 	"github.com/twilio/twilio-go"
 	twilioApi "github.com/twilio/twilio-go/rest/api/v2010"
+	"time"
 
 	"log"
 	"net/http"
 	"os"
 	"strings"
-	"time"
 )
 
 type tapWaterStartTime struct {
@@ -35,16 +34,16 @@ type tapWater struct {
 
 var ginLambda *ginadapter.GinLambda
 
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Printf("Processing Lambda request %s\n", req.RequestContext.RequestID)
-	return ginLambda.ProxyWithContext(ctx, req)
-}
-
 const ACCOUNT_SID_ENV = "TWILIO_ACCOUNT_SID"
 const ACCOUNT_AUTH_TOKEN_ENV = "TWILIO_AUTH_TOKEN"
 
 var debug string
 var enableSMS string
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("Processing Lambda request %s\n", req.RequestContext.RequestID)
+	return ginLambda.ProxyWithContext(ctx, req)
+}
 
 func init() {
 	err := godotenv.Load()
@@ -70,30 +69,30 @@ func main() {
 	}
 }
 
-func TapWaterHandler(ctx *gin.Context) {
-	const tableName = "tapwaterdb"
-	id := uuid.New()
-
+func createSupabaseClient() (*supabase.Client, error) {
 	supabaseUrl := os.Getenv("SUPABASE_URL")
 	supabaseKey := os.Getenv("SUPABASE_KEY")
+	return supabase.NewClient(supabaseUrl, supabaseKey, nil)
+}
 
+func TapWaterHandler(ctx *gin.Context) {
+	const tableName = "tapwaterdb"
 	var record tapWater
+
 	if err := ctx.BindJSON(&record); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	log.Printf("Record: %+v", record)
-
-	supabaseClient, err := supabase.NewClient(supabaseUrl, supabaseKey, nil)
+	supabaseClient, err := createSupabaseClient()
 	if err != nil {
-		log.Fatal("cannot connect to Supabase")
+		ctx.JSON(http.StatusInternalServerError, gin.H{"cannot connect to Supabase": err.Error()})
+		return
 	}
 
-	// form message to send SMS
-	duration := fmt.Sprint(record.Duration)
-	message := "\nDate: " + record.Date + "\nStart Time: " + record.StartTime + "\nEnd Time: " + record.EndTime + "\nDuration: " + duration + " minutes"
-	_ = sendSMS(message)
+	log.Printf("Record: %+v", record)
+
+	sendDepartureSMS(record)
 
 	// upsert record to the database
 	_, _, err = supabaseClient.From(tableName).Upsert(record, "", "", "").Eq("start_time", record.StartTime).Execute()
@@ -111,25 +110,23 @@ func TapWaterHandler(ctx *gin.Context) {
 		}
 	}
 
-	log.Printf("Record inserted successfully")
-	ctx.JSON(http.StatusOK, id)
+	log.Printf("record inserted successfully")
+	ctx.JSON(http.StatusCreated, "")
 }
 
 func TapWaterStartGetHandler(ctx *gin.Context) {
 	const tableName = "start_time"
 
-	supabaseUrl := os.Getenv("SUPABASE_URL")
-	supabaseKey := os.Getenv("SUPABASE_KEY")
-
 	var record tapWaterStartTime
 	date := ctx.Query("date")
-	supabaseClient, err := supabase.NewClient(supabaseUrl, supabaseKey, nil)
+
+	supabaseClient, err := createSupabaseClient()
 	if err != nil {
-		log.Fatal("cannot connect to Supabase")
+		ctx.JSON(http.StatusInternalServerError, gin.H{"cannot connect to Supabase": err.Error()})
+		return
 	}
 
 	res, err := supabaseClient.From(tableName).Select("*", "", false).Eq("date", date).Single().ExecuteTo(&record)
-
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -148,21 +145,13 @@ func TapWaterStartHandler(ctx *gin.Context) {
 		return
 	}
 
-	supabaseUrl := os.Getenv("SUPABASE_URL")
-	supabaseKey := os.Getenv("SUPABASE_KEY")
+	sendArrivalSMS()
 
-	supabaseClient, err := supabase.NewClient(supabaseUrl, supabaseKey, nil)
+	supabaseClient, err := createSupabaseClient()
 	if err != nil {
-		log.Fatal("cannot connect to Supabase")
+		ctx.JSON(http.StatusInternalServerError, gin.H{"cannot connect to Supabase": err.Error()})
+		return
 	}
-
-	// form message to send SMS
-	// get time in IST timezone like hh:mm:ss
-	location, _ := time.LoadLocation("Asia/Kolkata")
-	timeInIST := time.Now().In(location)
-
-	message := "\nWater has arrived on " + timeInIST.Format("03:04:05 PM")
-	_ = sendSMS(message)
 
 	_, _, err = supabaseClient.From(tableName).Insert(record, false, "", "", "").Execute()
 	if err != nil {
@@ -174,13 +163,33 @@ func TapWaterStartHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"message": "record inserted successfully"})
 }
 
+func sendArrivalSMS() {
+	location, _ := time.LoadLocation("Asia/Kolkata")
+	timeInIST := time.Now().In(location)
+
+	message := "\nWater has arrived on " + timeInIST.Format("03:04:05 PM")
+	err := sendSMS(message)
+	if err != nil {
+		log.Printf("error sending arrival sms: %v", err)
+	}
+}
+
+func sendDepartureSMS(record tapWater) {
+	duration := fmt.Sprint(record.Duration)
+	message := "\nDate: " + record.Date + "\nStart Time: " + record.StartTime + "\nEnd Time: " + record.EndTime + "\nDuration: " + duration + " minutes"
+	err := sendSMS(message)
+	if err != nil {
+		log.Printf("error sending departure sms: %v", err)
+	}
+}
+
 // send sms using twilio
 func sendSMS(message string) error {
 	if enableSMS == "true" {
 		accountSid := os.Getenv(ACCOUNT_SID_ENV)
 		authToken := os.Getenv(ACCOUNT_AUTH_TOKEN_ENV)
 		if accountSid == "" || authToken == "" {
-			log.Fatal("Twilio account SID or auth token not found")
+			log.Fatal("twilio account SID or auth token not found")
 		}
 
 		client := twilio.NewRestClientWithParams(twilio.ClientParams{
@@ -206,10 +215,10 @@ func sendSMS(message string) error {
 
 			resp, err := client.Api.CreateMessage(&params)
 			if err != nil {
-				fmt.Println("Error sending SMS message: " + err.Error())
+				fmt.Println("error sending sms message: " + err.Error())
 			} else {
 				response, _ := json.Marshal(*resp)
-				fmt.Println("Response: " + string(response))
+				fmt.Println("response: " + string(response))
 			}
 		}
 	}
